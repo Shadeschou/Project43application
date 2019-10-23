13b5389a6de02c16a45c6a778ee930daa2ef1c8a
Kasper Jørgensen <44469647+Shadeschou@users.noreply.github.com>
2019-10-24 00:37:45 +0200

    Merging-classes (#5)

--- WaterBillingSystem/AddCustomer.java
+++ WaterBillingSystem/AddCustomer.java
@@ -1,37 +1,57 @@
 import java.util.Scanner;
 
 public class AddCustomer {
-    public static void main(String[] args) {
+    public static int addCustomer() {
+        int chosenMenu = -1;
+
+        //create Scanner
+        Scanner in = new Scanner(System.in);
+        in.useDelimiter("\n");
+
+        System.out.println("The following functions can be found in the customer manager\n" +
+                "1. Add Customer\n" +
+                "press 0 to go back");
+
+        while (chosenMenu == -1) {
+
+            if (in.hasNextInt()) {
+                chosenMenu = in.nextInt();
+                if (chosenMenu == 0) {
+                    return -1;
+                } else if (chosenMenu != 1) {
+                    chosenMenu = -1;
+                }
+            } else {
+                System.out.println("Input doesn't match menu number try number 1 or 0 for going back");
+            }
+        }
 
 
-    //add new customer
-
-    //create Scanner
-    Scanner in = new Scanner(System.in);
-    in.useDelimiter( "\n");
-
-    //hold data for new customer in variables
-    System.out.println("Please input customer ID No:");
-    int customerId = in.nextInt();
-
-    System.out.println ("Please input customer type: 1, 2, or 3 (1=agriculture, 2=industry, 3=private household)");
-    int segmentType = in.nextInt();
-
-    System.out.println("Please input street and House No:");
-    String customerAddress = in.next();
-
-    System.out.println("Please input zipCode:");
-    int zipCode = in.nextInt();
-
-    System.out.println("Please input customer name:");
-    String customerName = in.next();
-
-    System.out.println("Please input customer Email:");
-    String customerEmail = in.next();
+        //add new customer
 
 
-    //insert data for new custromer into Database
-    DB.insertSQL("insert into customer values ('"+customerId+"', '"+segmentType+"', '"+customerAddress+"', '"+zipCode+"', '"+customerName+"', '"+customerEmail+"', '1' )");
+        //hold data for new customer in variables
+        System.out.println("Please input customer ID No:");
+        int customerId = in.nextInt();
 
+        System.out.println("Please input customer type: 1, 2, or 3 (1=agriculture, 2=industry, 3=private household)");
+        int segmentType = in.nextInt();
+
+        System.out.println("Please input street and House No:");
+        String customerAddress = in.next();
+
+        System.out.println("Please input zipCode:");
+        int zipCode = in.nextInt();
+
+        System.out.println("Please input customer name:");
+        String customerName = in.next();
+
+        System.out.println("Please input customer Email:");
+        String customerEmail = in.next();
+
+
+        //insert data for new custromer into Database
+        DB.insertSQL("insert into customer values ('" + customerId + "', '" + segmentType + "', '" + customerAddress + "', '" + zipCode + "', '" + customerName + "', '" + customerEmail + "', '1' )");
+        return 0;
     }
 }
